d9e4e6a423817e79c8813d19f0bb09599ee78416
QKRTJDUGS <bagseonghuyn0505@gmail.com>
2026-03-31 10:03:01 +0900

    Auto Push: Stringlog.java

---
+++ src/main/java/PracticeStudy/Stringlog.java
@@ -0,0 +1,8 @@
+public class Stringlog {
+    public static void main(String [] args){
+
+        String log = "127.0.0.1 - - [31/Mar/2026:10:00:01 +0900] \"GET /index.html HTTP/1.1\" 200 1043 \"-\" \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36\" ";
+        System.out.println(log.indexOf("\"" ));
+
+        int start = log.indexOf("\"");
+        int end = log.indexOf("\"", start+1);
